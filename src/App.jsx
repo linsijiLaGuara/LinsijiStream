a84189d09e9f9115f4b1c71f2644ebba33a52e37
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-07-03 19:04:28 -0400

    Base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import LinsijiStreamContainer from './contenedor/Contenedor'
+
+function App() {
+  return (
+    <>
+      <LinsijiStreamContainer />
+    </>
+  );
+}
+
+export default App;
