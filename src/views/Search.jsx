9c08f9c86a6a2938f4cbb7bcec08e9df6fe65ca7
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-07-05 17:21:46 -0400

    update: buscar

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import albums_mockup from "../mockupServices/albumList.json";
 import "./Search.css";
+import Sidebarwolcomen from "../components/Sidebarwolcomen";
 
 const Search = () => {
   const [albums, setAlbums] = useState([]);
@@ -10,22 +11,25 @@ const Search = () => {
   }, []);
 
   return (
-    <div className="search-container">
-      <h1>Buscar</h1>
-      <div className="albums-list">
-        {albums.map((album, index) => (
-          <div key={index} className="album-item">
-            <img
-              src={album.url_image}
-              alt={album.nombre_album}
-              className="album-image"
-            />
-            <div className="album-details">
-              <h2>{album.nombre_album}</h2>
-              <p>{album.artist_name}</p>
+    <div className="search-page">
+      <Sidebarwolcomen />
+      <div className="search-container">
+        <h1>Buscar</h1>
+        <div className="albums-list">
+          {albums.map((album, index) => (
+            <div key={index} className="album-item">
+              <img
+                src={album.url_image}
+                alt={album.nombre_album}
+                className="album-image"
+              />
+              <div className="album-details">
+                <h2>{album.nombre_album}</h2>
+                <p>{album.artist_name}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
